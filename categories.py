e83a0eb7bd3190a7f03fb485110bcd088389e2f0
Paul Hager <hagerpa@gmail.com>
2016-11-11 16:50:23 +0100

    Work a lot today. Created a class called categories which manages and initially also reads all categories from file. Used this in the processing notbook and build the first very simple classifier. Next thing to do is definetly to filter out important words. Fist thing to write in the Report is also the inequality in examples per class (wich can be eeasily ploted by nltk object FreqDist).

---
+++ categories.py
@@ -0,0 +1,115 @@
+import csv
+import re
+
+class categories:
+    
+    def __init__(self):
+        self.cats = {}
+        self.subcats = {}
+        self.loadfromfile()
+    
+    def loadfromfile(self, print_changes=False):
+        # opening original csv file
+        file = open('category.csv', 'r')
+        file.seek(0)
+        file_content = file.read()
+        file.close()
+    
+        # list of regular expressions that should be subtituted prior to read via CSV-reader
+        regexps = [(r"\\\"", "'"), 
+                   (re.compile(r"<span.*?/span>", re.DOTALL), ""),
+                   (r"<p.*?>", ""),
+                   (r"</p>", "")]
+    
+        # find an replace for each of the expressions in upper list
+        for regex in regexps:
+            find, replace = regex # f: the tobe Found expression, r: the expression which sill substitute
+            file_content, n = re.subn(find, replace, file_content)
+            if print_changes:
+                print("replaced expression {0} by expression {1} - {2} times.".format(find,replace,n))
+    
+        # saving into a new CSV file called: category_clean.csv
+        file_clean = open('category_clean.csv', 'w+', encoding="utf8")
+        file_clean.write(file_content)
+        file_clean.close()
+        
+        # return the already opend new category_clean.csv file and a CSV reader on that file
+        file = open('category_clean.csv', 'r')
+        reader = csv.reader(file)
+        self.cats, self.subcats = makedict(file, reader)
+        file.close()
+    
+    def __str__(self):
+        # Prints all categories with subcategories intendet after perent category
+        out = ""
+        for cat_id, cat in self.cats.items():
+            out += str(cat_id) + " " + str(cat[0]) + "\n"
+            for subcat in cat[1].items():
+                out += "\t" + str(subcat[0]) + " " + str(subcat[1]) + "\n"
+            out += "\n"
+        return out
+    
+    def name(self, id_):
+        # returns the category name of the parent for a given category id
+        if id_ in self.cats.keys():
+            return self.cats[id_][0]
+        elif id_ in self.subcats.keys():
+            return self.name(self.subcats[id_])
+        else:
+            raise ValueError("There is no category with the id", id_)
+    
+    def parent_id(self, id_):
+        if id_ in self.subcats.keys():
+            return self.subcats[id_]
+        else:
+            raise ValueError("There is no category with the id", id_)
+
+
+def makedict(catfile, catreader):
+    # Method that creats a dict which has the parent category id as keys, and as values it hast a Tuple
+    # consisting of 1. the name of the parent category, 2. a dictionarry with subcategorys
+    # these are of the form (key:category_id, value:categoryname)
+    catfile.seek(0)
+
+    #skipping column discription:
+    next(catreader)
+
+    category_dict = {} # Dictionary which has as key: category_id, ...
+    #..- and as values: (category_feature_name, dictionary of subcategories, category_name)
+
+    subcat = {} # this is a mapping from cadigory ids to parent ids, if it category is a parent
+    #.. it'll be the identity
+
+    for row in catreader:
+        if int(row[1]) == 0:
+            category_dict[int(row[0])] = [make_feature_name(row[2]), {}, row[2]]
+            subcat[int(row[0])] = int(row[0])
+        else:
+            subcat[int(row[0])] = int(row[1])
+        
+    catfile.seek(0)
+    next(catreader)
+
+    for row in catreader:
+        if int(row[1]) != 0:
+            category_dict[int(row[1])][1][int(row[0])] = row[2]
+
+    return category_dict, subcat
+
+def make_feature_name(name):
+    featurename = name
+    regexps = [(r" ", "_"),
+                (r"ö","oe"),
+                (r"ä","ae"),
+                (r"ü","ue"),
+                (r"&","and")]
+    for find, replace in regexps:
+        featurename = re.sub(find, replace, featurename)
+    return featurename.lower()
+
+
+
+
+
+
+
